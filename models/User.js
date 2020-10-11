a9c5070dc5b0d4c33503d5934910aa6124c7d86a
이병준 <bjun@ibyeongjun-ui-MacBookPro.local>
2020-10-11 11:27:48 +0900

    First Update to Git Storage

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    maxlength: 50,
+    trim: true,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: {
+    tpye: String,
+  },
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User };
